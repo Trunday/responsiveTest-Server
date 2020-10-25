633a4e9bec355beff29bf42fa84778f024c23264
Trunday <musqa25@hotmail.com>
2020-10-25 13:34:28 +0300

    degişiklik var gibi

--- src/routes/auth/auth.router.js
+++ src/routes/auth/auth.router.js
@@ -3,6 +3,7 @@ import config from 'config';
 const route = () => {
     const router = new express.Router();
     router.route('/login').post((req,res)=>{
+    // router.route('/login').get((req,res)=>{ // get metotunda test ettim
         res.send("ben tamamım");
     })
     return router;
